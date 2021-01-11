5c87978a013230441af3b7824082c136835adfdf
Administrator <3241914864@qq.com>
2021-01-11 19:57:15 +0800

    飞机订票系统 订票

---
+++ src/Order.java
@@ -0,0 +1,66 @@
+public class Order {
+    private String id;
+    private float jpjg;//机票价格
+    private float airPortTax;//机场税
+    private float hkzhx;//航空综合险
+    private float jptgx;//机票退改险
+    private float yhq;//优惠券
+    private float hszj;//含税总价
+    private passenger passengers;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public float getJpjg() {
+        return jpjg;
+    }
+
+    public void setJpjg(float jpjg) {
+        this.jpjg = jpjg;
+    }
+
+    public float getAirPortTax() {
+        return airPortTax;
+    }
+
+    public void setAirPortTax(float airPortTax) {
+        this.airPortTax = airPortTax;
+    }
+
+    public float getHkzhx() {
+        return hkzhx;
+    }
+
+    public void setHkzhx(float hkzhx) {
+        this.hkzhx = hkzhx;
+    }
+
+    public float getJptgx() {
+        return jptgx;
+    }
+
+    public void setJptgx(float jptgx) {
+        this.jptgx = jptgx;
+    }
+
+    public float getYhq() {
+        return yhq;
+    }
+
+    public void setYhq(float yhq) {
+        this.yhq = yhq;
+    }
+
+    public float getHszj() {
+        return hszj;
+    }
+
+    public void setHszj(float hszj) {
+        this.hszj = hszj;
+    }
+}
