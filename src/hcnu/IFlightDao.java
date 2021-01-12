89d606019cb1b01173393a44dd8fe8796c14050a
Administrator <3241914864@qq.com>
2021-01-12 11:47:03 +0800

    飞机订票系统 修改订票信息

---
+++ src/hcnu/IFlightDao.java
@@ -0,0 +1,14 @@
+package hcnu;
+
+import hcnu12.Flight;
+
+import java.util.Set;
+
+public interface IFlightDao {
+    void insertFlight(Flight flight);
+    Set<Flight> getAllFlights();
+    Flight getFlightByDepartureTime(String departureTime);
+    Flight getFlightByDepartureAirPort(String departureAirPort);
+    Flight getFlightByDestinationAirPort(String destinationAirPort);
+    void updateFlight(Flight flight);
+}
